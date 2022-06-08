f26d8c1384d7d94740e74609809eb7a18cbc4976
jojeongbin <cjb1009@naver.com>
2022-06-09 02:02:05 +0900

    2주차 과제 수정사항(1)

---
+++ thymeleaf_2nd/thymeleaf_template/src/test/java/spirng_study1/thymeleaf/ThymeleafTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package spirng_study1.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
