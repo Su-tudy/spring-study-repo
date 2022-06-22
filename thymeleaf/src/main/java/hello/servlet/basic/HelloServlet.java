f222d385a9b8c4c348369e8e8c91cf0ee7535c70
김동열 <ehfql6363@naver.com>
2022-06-22 11:36:38 +0900

    FEAT : 4회차 HttpServlet

--- thymeleaf/src/main/java/hello/servlet/basic/HelloServlet.java
+++ thymeleaf/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -1,2 +1,28 @@
-package hello.servlet.basic;public class HelloServlet {
+package hello.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet; //servlet은 HttpServlet을 상속받음
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello") //servlet이름과 패턴은 겹치면 안됨.
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //서블릿이 호출되면 service메소드 실행
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello " + username);
+
+    }
 }
