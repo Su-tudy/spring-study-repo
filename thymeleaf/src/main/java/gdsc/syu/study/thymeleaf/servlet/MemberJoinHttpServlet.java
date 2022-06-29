3b3d6f0bd31fefbf0a31f281c274c1bf8a79acc5
JJIMINSHIN <86641936+JJIMINSHIN@users.noreply.github.com>
2022-06-29 22:51:26 +0900

    Update MemberJoinHttpServlet.java

--- thymeleaf/src/main/java/gdsc/syu/study/thymeleaf/servlet/MemberJoinHttpServlet.java
+++ thymeleaf/src/main/java/gdsc/syu/study/thymeleaf/servlet/MemberJoinHttpServlet.java
@@ -8,6 +8,20 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 // urlPatterns = "/memberjoinhttpservlet"
+// urlPatterns = "/memberjoinhttpservlet"
+@WebServlet(name = "memberJoinHttpServlet", urlPatterns = "/memberjoinhttpservlet")
 public class MemberJoinHttpServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String age = req.getParameter("age");
 
+        if(username.length() > 0 && username !="" && age.length() >0 && age !="") {
+            System.out.println("회원가입이 되었습니다.");
+            System.out.println("username : " + username);
+            System.out.println("age : " + age);
+        }else {
+            resp.getWriter().write(400);
+        }
+    }
 }
