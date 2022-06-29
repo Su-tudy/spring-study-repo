454262cb964ff07d16c0366effebbdd9d69e5cd2
hyena0608 <goatsfish@gmail.com>
2022-06-29 20:52:47 +0900

    [4회차 과제] : feat : HttpServlet, GenericServlet 회원 가입 출력 구현

--- thymeleaf/src/main/java/gdsc/syu/study/thymeleaf/servlet/MemberJoinGenericServlet.java
+++ thymeleaf/src/main/java/gdsc/syu/study/thymeleaf/servlet/MemberJoinGenericServlet.java
@@ -5,9 +5,25 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 // urlPatterns = "/memberjoingenericservlet"
+@WebServlet(name = "memberJoinGenericServlet", urlPatterns = "/memberjoingenericservlet")
 public class MemberJoinGenericServlet extends GenericServlet {
 
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String age = req.getParameter("age");
+
+        if (username.length() > 0 && username != "" && age.length() > 0 && age != "") {
+            System.out.println("회원 가입이 완료 되었습니다.");
+            System.out.println("username = " + username);
+            System.out.println("age = " + age);
+            res.getWriter().write(HttpServletResponse.SC_OK);
+        } else if (username.length() == 0 || username == "" || age.length() == 0 || age == "") {
+            res.getWriter().write(HttpServletResponse.SC_BAD_REQUEST);
+        }
+    }
 }
--- thymeleaf/src/main/java/gdsc/syu/study/thymeleaf/servlet/MemberJoinHttpServlet.java
+++ thymeleaf/src/main/java/gdsc/syu/study/thymeleaf/servlet/MemberJoinHttpServlet.java
@@ -8,6 +8,22 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 // urlPatterns = "/memberjoinhttpservlet"
+@WebServlet(name = "memberJoinHttpServlet", urlPatterns = "/memberjoinhttpservlet")
 public class MemberJoinHttpServlet extends HttpServlet {
 
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String age = req.getParameter("age");
+
+        if (username.length() > 0 && username != "" && age.length() > 0 && age != "") {
+            System.out.println("회원 가입이 완료 되었습니다.");
+            System.out.println("username = " + username);
+            System.out.println("age = " + age);
+            resp.getWriter().write(HttpServletResponse.SC_OK);
+        } else if (username.length() == 0 || username == "" || age.length() == 0 || age == "") {
+            resp.getWriter().write(HttpServletResponse.SC_BAD_REQUEST);
+        }
+
+    }
 }
