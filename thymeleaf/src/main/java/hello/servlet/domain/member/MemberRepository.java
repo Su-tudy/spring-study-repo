13e357051c47022f3718f81047122f2ec491c2af
dong yeol <ehfql6363@naver.com>
2022-07-05 17:57:29 +0900

    FEAT : 6회차 ~JSP
    Signed-off-by: dong yeol <ehfql6363@naver.com>

---
+++ thymeleaf/src/main/java/hello/servlet/domain/member/MemberRepository.java
@@ -0,0 +1,49 @@
+package hello.servlet.domain.member;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 동시성 문제가 고려되어 있지 않음, 실무에서는 ConcurrentHashMap, AtomicLong 사용 고려
+ * 동시성이란,
+ */
+
+public class MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    //싱글톤
+    private static final MemberRepository instance = new MemberRepository();
+
+    //MemberRepository의 getInstance()를 통해서 instance객체 확인 가능.
+    public static MemberRepository getInstance() {
+        return instance;
+    }
+
+    //싱글톤에 맞게 private으로 생성자 생성
+    //왜?
+    private MemberRepository(){
+    }
+
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Member> findAll() {
+        //store의 값들을 전부 꺼내 새로운 ArrayList에 담아서 return
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
---
+++ thymeleaf/src/test/java/hello/servlet/ServletApplicationTest.java
@@ -0,0 +1,7 @@
+package hello.servlet;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServletApplicationTest {
+
+}
