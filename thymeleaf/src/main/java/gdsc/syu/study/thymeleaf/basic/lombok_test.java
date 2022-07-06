edbfa7ea8e6e74bfc7e21ce25f1833a4af2f7870
hyena0608 <goatsfish@gmail.com>
2022-07-06 11:16:29 +0900

    [5회차 과제] : feat : lombok getter setter annotation

---
+++ thymeleaf/src/main/java/gdsc/syu/study/thymeleaf/basic/lombok_test.java
@@ -0,0 +1,11 @@
+package gdsc.syu.study.thymeleaf.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class lombok_test {
+    private String username;
+    private String userDept;
+    private int userID;
+}
