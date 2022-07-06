cb4b7a67fdaa3a621d02452e1aac45f89aa5e898
dong yeol <ehfql6363@naver.com>
2022-07-06 18:05:34 +0900

    FEAT : 5회차 과제
    Signed-off-by: dong yeol <ehfql6363@naver.com>

---
+++ thymeleaf/src/main/java/hello/servlet/basic/homeWork/LombokTest.java
@@ -0,0 +1,11 @@
+package hello.servlet.basic.homeWork;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class LombokTest {
+        private String username;
+        private String userDept;
+        private int userID;
+}
